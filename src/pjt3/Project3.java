bf2c68b710c068328f7248266d071b35fe703e5f
Kelait <collin.patteson@gmail.com>
2017-08-03 23:32:22 -0500

    Update Project3.java Header

--- src/pjt3/Project3.java
+++ src/pjt3/Project3.java
@@ -1,3 +1,31 @@
+//
+// Title:            Project 3
+//
+// Files:            Project3.java, Game.java, WordGenerator.java, UserInputNotFoundException.java, InvalidUserInputException.java, GameOverException.java,
+//					 QuitGameException.java.
+// Other Dependencies: words.txt, engLocal.txt
+// Semester:         CS302 Summer 2016
+//
+// Author:           Collin Patteson
+// Email:            cPatteson@wisc.edu
+// Lecturer's Name:  Steve Earth
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+//
+// Partner Name:     Joong Ho Kim
+// Partner Email:    jkim574@wisc.edu
+
+// Lecturer's Name:  Steve Earth
+
+// 
+// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
+//   X ___ Write-up states that Pair Programming is allowed for this assignment.
+//   X ___ We have both read the CS302 Pair Programming policy.
+//   X ___ We have registered our team prior to the team registration deadline.
+//
+
+
+
 package pjt3;
 import java.io.*;
 import java.util.*;
